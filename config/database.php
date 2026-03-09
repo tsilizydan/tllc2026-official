c62968f449db96dda803d2b0ad9144e687e6373d
tsilizydan <150443526+tsilizydan@users.noreply.github.com>
2026-03-09 17:49:23 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -3,10 +3,10 @@
  * TSILIZY LLC — Database Configuration
  */
 
-define('DB_HOST', 'sql105.byethost7.com');
-define('DB_NAME', 'b7_40611962_tsilizy_llc');
-define('DB_USER', 'b7_40611962');
-define('DB_PASS', 'RamTsida@2898');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'tsilscpx_tsilizy_llc');
+define('DB_USER', 'tsilscpx_chibi_admin');
+define('DB_PASS', '9@UPN~I@O]Dw');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATION', 'utf8mb4_unicode_ci');
 
